23a51ce1f9c750e8d14c6bf0a94b739d33bce9f7
KhineWin <khinewin123@gmail.com>
2025-07-16 09:18:06 +0630

    start project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template lang="">
+  <div>
+    <h1>About</h1>
+    <router-link :to="{name: 'Home'}">Home</router-link>
+    
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="">
+  
+</style>
---
+++ src/views/UserView.vue
@@ -0,0 +1,17 @@
+<template lang="">
+    <div>
+        <h1>User Page {{user_id}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            user_id : this.$route.params.user_id,
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
