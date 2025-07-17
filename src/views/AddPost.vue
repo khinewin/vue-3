0156bd4bdab9a0d9f45d228f000792dc66fcd139
KhineWin <khinewin123@gmail.com>
2025-07-17 09:28:04 +0630

    save post

--- src/views/AddPost.vue
+++ src/views/AddPost.vue
@@ -1,14 +1,16 @@
 <template lang="">
     <div>
+        <div v-if="isLoading">Loading...</div>
+         <div v-if="isSuccess">The post has been created</div>
         <h1>Add post</h1>
         <form @submit.prevent="savePost">
-            <span v-if="error.title">{{error.title}}</span>
-            <label for="title">Titlte</label>
-            <input type="text" id="title" v-model="title">
+            <div v-if="error.title">{{error.title}}</div>
+            <label for="title">Title</label>
+            <input type="text" id="title" v-model="title" @input="resetError('title')">
             <br>
-            <span v-if="error.content">{{error.content}}</span>
+            <div v-if="error.content">{{error.content}}</div>
             <label for="content">Content</label>
-            <textarea  id="content" v-model="content"></textarea>
+            <textarea  id="content" v-model="content" @input="resetError('content')"></textarea>
             <br>
             <button type="submit">Save post</button>
         </form>
@@ -25,26 +27,45 @@ export default {
             error: {
                 title:"",
                 content:""
-            }
+            },
+            isLoading: false,
+            isSuccess:false,
         }
     },
     methods:{
+        resetError(data){
+            if(data=="title"){
+                this.error.title="";
+            }else  if(data=="content"){
+                this.error.content="";
+            }
+        },
         savePost(){
+            this.isLoading=true;
             const data={
                 title: this.title,
                 content: this.content
             }
             axios.post(`${this.server_addr}/api/posts`, data )
             .then((res)=>{
+                this.isLoading=false;
+                if(res.data.status=="success"){
+                    this.isSuccess=true;
+                    this.title="";
+                    this.content="";
+                    setTimeout(()=>{
+                        this.isSuccess=false;
+                    }, 3000)
+                }
               console.log(res)
             })
             .catch((err)=>{
-
+                this.isLoading=false;
                 if(err.status==422){
-                    if(err.response.data.errors.title.length){
+                    if(err.response.data.errors.title){
                             this.error.title=err.response.data.errors.title[0]
                     }
-                    if(err.response.data.errors.content.length){
+                    if(err.response.data.errors.content){
                             this.error.content=err.response.data.errors.content[0]
                     }
 
