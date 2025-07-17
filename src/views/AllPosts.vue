5efbcbc336adbe8831ef8c67cde41e569f14e33a
KhineWin <khinewin123@gmail.com>
2025-07-17 10:52:21 +0630

    delete post

--- src/views/AllPosts.vue
+++ src/views/AllPosts.vue
@@ -1,6 +1,8 @@
 <template lang="">
     <div>
         <Navbar />
+        <div v-if="isLoading">Loading...</div>
+        <div v-if="msg">{{msg}}</div>
         <h1>Get All Posts</h1>
         <table>
             <tr>
@@ -8,12 +10,16 @@
                 <td>Title</td>
                 <td>Content</td>
                 <td>Created Date</td>
+                <td>Actions</td>
             </tr>
             <tr v-for="p in posts" :key="p.id">
                 <td>{{p.id}}</td>
                 <td>{{p.title}}</td>
                 <td>{{p.content}}</td>
                 <td>{{p.created_at}}</td>
+                <td>
+                    <button type="button" @click="deletePost(p.id)">Delete</button>
+                </td>
             </tr>
         </table>
     </div>
@@ -28,21 +34,45 @@ export default {
         data(){
             return{
                 server_addr: "http://192.168.1.4:8000",
-                posts:[]
+                posts:[],
+                isLoading: false,
+                msg:null
             }
         },
         mounted(){
             this.fetchPosts();
         },
         methods:{
+            deletePost(id){
+                let sure=confirm(`Are you sure ? ,This will delete post with id : ${id}`);
+                if(sure){
+                    axios.delete(`${this.server_addr}/api/posts/id/${id}`)
+                    .then((res)=>{
+                        if(res.data.status=="success"){
+                            this.msg=res.data.msg;
+                            setTimeout(()=>{
+                                this.msg=null;
+                            }, 3000)
+                            this.posts=[]
+                            this.fetchPosts();
+                        }
+                    })
+                    .catch((err)=>{
+                        console.log(err)
+                    })
+                }
+            },
             fetchPosts(){
+                this.isLoading=true;
                 axios.get(`${this.server_addr}/api/posts`)
                 .then((res)=>{
                     this.posts=res.data;
+                    this.isLoading=false;
                    // console.log(res);
                 })
                 .catch((err)=>{
                     console.log(err)
+                    this.isLoading=false;
                 });
             }
         }
